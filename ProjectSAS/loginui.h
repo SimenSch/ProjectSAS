144cdac06e551cd97f22b6c984542717b8a0ea9b
SimenPA <simenp.andersen@gmail.com>
2018-04-10 13:57:20 +0200

    first batch of login UI

---
+++ ProjectSAS/loginui.h
@@ -0,0 +1,28 @@
+#ifndef LOGINUI_H
+#define LOGINUI_H
+
+#include <QWidget>
+#include <QLabel>
+#include <QSpinBox>
+#include <QToolButton>
+#include <QFrame>
+#include <QLineEdit>
+
+class LoginUI: public QWidget
+{
+     Q_OBJECT
+public:
+    LoginUI(QWidget *parent = 0);
+    ~LoginUI();
+private:
+    QLabel *welcomeLabel;
+    QLabel *userLabel;
+    QLabel *passwordLabel;
+
+    QLineEdit *userEdit;
+    QLineEdit *passwordEdit;
+
+
+};
+
+#endif // LOGINUI_H

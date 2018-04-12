ef1cc4dddc6c8173c57469f05a8c865213895802
chickdestroyer <s305491@stud.hioa.no>
2018-04-12 19:07:24 +0200

    Deleting non needed items and doing some other stuff  like getters and setters

--- ProjectSAS/owner.h
+++ ProjectSAS/owner.h
@@ -1,6 +1,6 @@
 #ifndef OWNER_H
 #define OWNER_H
-#include <string>;
+#include <string>
 #include <iostream>
 using namespace std;
 
@@ -9,6 +9,27 @@ class Owner
 {
 public:
     Owner();
+    int getownerID();
+    void setownerID(int newownerID);
+    string getsurname();
+    void setsurname(string newsurname);
+    string getfirstName();
+    void setfirstName(string newfirstName);
+    string getadress();
+    void setfirstname(string newfirstame);
+    string getaddress();
+    void setaddress(string newaddress);
+    string getcity();
+    void setcity(string newcity);
+    int getzip();
+    void setzip(int newzip);
+    string getdateOfBirth();
+    void setdateOfBirth(string newdateofbirth);
+    string getdepartment();
+    void setdepartment(string newdepartment);
+    string geteMail();
+    void seteMail(string neweMail);
+
 private:
     int ownerID;
     string surname;

863628c65294d197d8f4729fe6dd9f212c618ba9
AndersOlsen <anders.noss@gmail.com>
2018-04-18 14:44:49 +0200

    fixed create uash x5

--- build-ProjectSAS-Desktop_Qt_5_10_0_MinGW_32bit-Debug/debug/moc_application.cpp
+++ build-ProjectSAS-Desktop_Qt_5_10_0_MinGW_32bit-Debug/debug/moc_application.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Application_t {
-    QByteArrayData data[5];
-    char stringdata0[90];
+    QByteArrayData data[7];
+    char stringdata0[147];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -35,12 +35,16 @@ QT_MOC_LITERAL(0, 0, 11), // "Application"
 QT_MOC_LITERAL(1, 12, 22), // "on_loginButton_clicked"
 QT_MOC_LITERAL(2, 35, 0), // ""
 QT_MOC_LITERAL(3, 36, 27), // "on_switchUserButton_clicked"
-QT_MOC_LITERAL(4, 64, 25) // "on_loadPetsButton_clicked"
+QT_MOC_LITERAL(4, 64, 25), // "on_loadPetsButton_clicked"
+QT_MOC_LITERAL(5, 90, 31), // "on_cancelRegisterButton_clicked"
+QT_MOC_LITERAL(6, 122, 24) // "on_newUserButton_clicked"
 
     },
     "Application\0on_loginButton_clicked\0\0"
     "on_switchUserButton_clicked\0"
-    "on_loadPetsButton_clicked"
+    "on_loadPetsButton_clicked\0"
+    "on_cancelRegisterButton_clicked\0"
+    "on_newUserButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,7 +54,7 @@ static const uint qt_meta_data_Application[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       5,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,11 +62,15 @@ static const uint qt_meta_data_Application[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   29,    2, 0x08 /* Private */,
-       3,    0,   30,    2, 0x08 /* Private */,
-       4,    0,   31,    2, 0x08 /* Private */,
+       1,    0,   39,    2, 0x08 /* Private */,
+       3,    0,   40,    2, 0x08 /* Private */,
+       4,    0,   41,    2, 0x08 /* Private */,
+       5,    0,   42,    2, 0x08 /* Private */,
+       6,    0,   43,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -79,6 +87,8 @@ void Application::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id,
         case 0: _t->on_loginButton_clicked(); break;
         case 1: _t->on_switchUserButton_clicked(); break;
         case 2: _t->on_loadPetsButton_clicked(); break;
+        case 3: _t->on_cancelRegisterButton_clicked(); break;
+        case 4: _t->on_newUserButton_clicked(); break;
         default: ;
         }
     }
@@ -110,13 +120,13 @@ int Application::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 5)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 5;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 5)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 5;
     }
     return _id;
 }

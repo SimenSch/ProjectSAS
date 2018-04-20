ca5d75baf1ba7b012b6891a3462db1aae27f44f1
SimenPA <simenp.andersen@gmail.com>
2018-04-20 13:44:58 +0200

    user valg andshiz
    user type valg, automatisk load av pets, and sutff

--- ProjectSAS/application.h
+++ ProjectSAS/application.h
@@ -19,7 +19,7 @@ public:
 private slots:
     void on_loginButton_clicked();
     void on_switchUserButton_clicked();
-    void on_loadPetsButton_clicked();
+    void loadPets();
     void on_cancelRegisterButton_clicked();
     void on_newUserButton_clicked();
     void on_registerButton_clicked();
@@ -27,6 +27,32 @@ private slots:
     void on_addPetButton_clicked();
     void on_cancelPetAddButton_clicked();
 
+
+    void on_firstNameInput_textEdited();
+
+    void on_surNameInput_textEdited();
+
+    void on_dateOfBirthInput_textEdited();
+
+    void on_addressInput_textChanged();
+
+    void on_cityInput_textEdited();
+
+    void on_zipInput_textEdited();
+
+    void on_phoneinput_textEdited();
+
+    void on_eMailInput_textEdited();
+
+    void on_passwordInput_textEdited();
+
+    void on_reEnterPasswordInput_textEdited();
+
+    void on_cancelUserChoiceButton_clicked();
+
+    void on_chooseUserTypeButton_clicked();
+
+    void on_customerTab_currentChanged(int index);
 
 private:
     Ui::Application *ui;

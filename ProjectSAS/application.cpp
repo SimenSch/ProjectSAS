3bb748036b2c76a5ffdd9365a8d7ae96675a131e
AndersOlsen <anders.noss@gmail.com>
2018-04-20 13:05:18 +0200

    login error shits fixed

--- ProjectSAS/application.cpp
+++ ProjectSAS/application.cpp
@@ -35,11 +35,9 @@ void Application::on_loginButton_clicked() {
 
 
     LoginInterface li;
-    cout << "login attempt return: " << li.loginAttempt(ui->userNameEdit->text().toStdString(), ui->passwordEdit->text().toStdString());
     if(li.loginAttempt(ui->userNameEdit->text().toStdString(), ui->passwordEdit->text().toStdString()) == 99)
     {
         activeUser.setuserID(li.getUserID(ui->userNameEdit->text().toStdString()));
-        cout << "user type fra loginbutton clicked: " << li.getUserType(activeUser.getuserID()) << endl;
         if(li.getUserType(activeUser.getuserID()).compare("Customer")) {
             ui->stackedWidget->setCurrentIndex(1);
             ui->mainStack->setCurrentIndex(0);
@@ -52,6 +50,9 @@ void Application::on_loginButton_clicked() {
             ui->errorLabel->setText("Error finding usertype");
             ui->errorLabel->show();
         }
+    } else if (li.loginAttempt(ui->userNameEdit->text().toStdString(), ui->passwordEdit->text().toStdString()) == 2){
+        ui->errorLabel->setText("Fields cannot be empty");
+        ui->errorLabel->show();
     }
     else {
         ui->errorLabel->setText("No matching user/password");
